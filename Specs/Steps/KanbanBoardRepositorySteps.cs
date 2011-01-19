c2a8ab9f0f45369b7ff7a674f856c8ab710f16aa
Darren Cauthon <darren@cauthon.com>
2011-01-19 06:33:07 -0600

    Splits the steps into separate files, one for each class that the steps relate to.

---
+++ Specs/Steps/KanbanBoardRepositorySteps.cs
@@ -0,0 +1,23 @@
+﻿using NSubstitute;
+using TechTalk.SpecFlow;
+using TechTalk.SpecFlow.Assist;
+using Web.Models.Domain;
+using Web.Storage;
+
+namespace Specs.Steps
+{
+    [Binding]
+    public class KanbanBoardRepositorySteps
+    {
+        [Given(@"the following Kanbanboards")]
+        public void GivenTheFollowingKanbanBoards(Table table)
+        {
+            var kanbanBoards = table.CreateSet<KanbanBoard>();
+
+            var mockedRepository = Substitute.For<IKanbanBoardRepository>();
+            mockedRepository.GetAllKanbanBoards().Returns(kanbanBoards);
+
+            ScenarioContext.Current.Set(mockedRepository);
+        }
+    }
+}
---
+++ Specs/Steps/MvcSteps.cs
@@ -0,0 +1,20 @@
+﻿using System.Web.Mvc;
+using Should.Fluent;
+using TechTalk.SpecFlow;
+
+namespace Specs.Steps
+{
+    [Binding]
+    public class MvcSteps
+    {
+        [Then(@"I should be on the (.*) page")]
+        public void ThenIShouldBeOnTheIndexPage(string viewName)
+        {
+            var actionResult = ScenarioContext.Current.Get<ActionResult>();
+            actionResult.Should().Be.OfType(typeof (ViewResult));
+
+            var viewResult = actionResult as ViewResult;
+            viewResult.ViewName.Should().Equal(viewName);
+        }
+    }
+}
