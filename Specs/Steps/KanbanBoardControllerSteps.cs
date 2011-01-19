b26a03b2120d37686510bc85816423e8b2cae7ab
Darren Cauthon <darren@cauthon.com>
2011-01-19 06:33:07 -0600

    Splits the steps into separate files, one for each class that the steps relate to.

---
+++ Specs/Steps/KanbanBoardControllerSteps.cs
@@ -0,0 +1,22 @@
+﻿using System.Web.Mvc;
+using TechTalk.SpecFlow;
+using Web.Controllers;
+using Web.Storage;
+
+namespace Specs.Steps
+{
+    [Binding]
+    public class KanbanBoardControllerSteps
+    {
+        [When(@"I to to the homepage")]
+        public void WhenIToToTheHomepage()
+        {
+            var kanbanBoardRepository = ScenarioContext.Current.Get<IKanbanBoardRepository>();
+            var kanbanBoardController = new KanbanBoardController(kanbanBoardRepository);
+
+            var viewResult = kanbanBoardController.Index();
+
+            ScenarioContext.Current.Set<ActionResult>(viewResult);
+        }
+    }
+}
---
+++ Specs/Steps/KanbanBoardIndexViewModelSteps.cs
@@ -0,0 +1,34 @@
+﻿using System.Linq;
+using System.Web.Mvc;
+using Should.Fluent;
+using TechTalk.SpecFlow;
+using TechTalk.SpecFlow.Assist;
+using Web.Models.Domain;
+using Web.Models.ViewModels;
+
+namespace Specs.Steps
+{
+    [Binding]
+    public class KanbanBoardIndexViewModelSteps
+    {
+        [Then(@"I should see the following Kanbanboards as the most favorited:")]
+        public void ThenIShouldSeeTheFollowingKanbanBoardsAsTheMostFavorited(Table table)
+        {
+            var viewResult = ScenarioContext.Current.Get<ActionResult>() as ViewResult;
+            var viewModel = viewResult.ViewData.Model as KanbanBoardIndexViewModel;
+
+            var boardsFromStep = table.CreateSet<KanbanBoard>().ToList();
+
+            var topFavoritedKanbanBoards = viewModel.TopFavoritedKanbanBoards;
+
+            topFavoritedKanbanBoards.Count.Should().Equal(boardsFromStep.Count);
+
+            for (var i = 0; i < table.RowCount; i++)
+            {
+                topFavoritedKanbanBoards[i].Title.Should().Equal(boardsFromStep[i].Title);
+                topFavoritedKanbanBoards[i].Posted.Should().Equal(boardsFromStep[i].Posted);
+                topFavoritedKanbanBoards[i].User.Should().Equal(boardsFromStep[i].User);
+            }
+        }
+    }
+}
