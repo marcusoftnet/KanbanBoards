40a53c0b128d1600a412a0b39bf16000c228b7e7
Darren Cauthon <darren@cauthon.com>
2011-01-19 06:37:34 -0600

    Update to 1.5 to use the table helpers.

--- Specs/Steps/KanbanBoardIndexViewModelSteps.cs
+++ Specs/Steps/KanbanBoardIndexViewModelSteps.cs
@@ -1,9 +1,6 @@
-﻿using System.Linq;
-using System.Web.Mvc;
-using Should.Fluent;
+﻿using System.Web.Mvc;
 using TechTalk.SpecFlow;
 using TechTalk.SpecFlow.Assist;
-using Web.Models.Domain;
 using Web.Models.ViewModels;
 
 namespace Specs.Steps
@@ -14,21 +11,17 @@ namespace Specs.Steps
         [Then(@"I should see the following Kanbanboards as the most favorited:")]
         public void ThenIShouldSeeTheFollowingKanbanBoardsAsTheMostFavorited(Table table)
         {
-            var viewResult = ScenarioContext.Current.Get<ActionResult>() as ViewResult;
-            var viewModel = viewResult.ViewData.Model as KanbanBoardIndexViewModel;
-
-            var boardsFromStep = table.CreateSet<KanbanBoard>().ToList();
+            var viewModel = GetTheViewResult();
 
             var topFavoritedKanbanBoards = viewModel.TopFavoritedKanbanBoards;
 
-            topFavoritedKanbanBoards.Count.Should().Equal(boardsFromStep.Count);
+            table.CompareToSet(topFavoritedKanbanBoards);
+        }
 
-            for (var i = 0; i < table.RowCount; i++)
-            {
-                topFavoritedKanbanBoards[i].Title.Should().Equal(boardsFromStep[i].Title);
-                topFavoritedKanbanBoards[i].Posted.Should().Equal(boardsFromStep[i].Posted);
-                topFavoritedKanbanBoards[i].User.Should().Equal(boardsFromStep[i].User);
-            }
+        private static KanbanBoardIndexViewModel GetTheViewResult()
+        {
+            var viewResult = ScenarioContext.Current.Get<ActionResult>() as ViewResult;
+            return viewResult.ViewData.Model as KanbanBoardIndexViewModel;
         }
     }
 }
