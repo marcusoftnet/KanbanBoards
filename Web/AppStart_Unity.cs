209489d1c3b351baa0454598c001c30b8348be48
marcushammarberg <marcusoft.net@gmail.com>
2011-01-29 11:02:26 +0100

    Showing the use of Unity as Dependency Resolver: http://www.marcusoft.net/2011/01/using-unity-as-dependency-resolver-in.html

---
+++ Web/AppStart_Unity.cs
@@ -0,0 +1,25 @@
+﻿using System.Web.Mvc;
+using Microsoft.Practices.Unity;
+using Web.Storage;
+
+//[assembly: WebActivator.PreApplicationStartMethod(typeof(Web.AppStart_Unity), "Start")]
+
+namespace Web
+{
+    public static class AppStart_Unity {
+        public static void RegisterServices(IUnityContainer container) {
+            container.RegisterType<IKanbanBoardRepository, KanbanBoardRepository>();
+        }
+
+        public static void Start() {
+            // Create Unity container
+            IUnityContainer container = new UnityContainer();
+
+            // Register services with our Unity Container
+            RegisterServices(container);
+
+            // Tell ASP.NET MVC 3 to use our Unity DI Container 
+            DependencyResolver.SetResolver(new UnityServiceLocator(container));
+        }
+    }
+}
